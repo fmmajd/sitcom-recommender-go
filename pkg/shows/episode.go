c9b61a502d0deb273fd988ebaad40835b02ee6dd
Fatemeh Majd <fmmajd452@gmail.com>
2020-05-03 03:11:22 +0430

    added custom Unmarshaler for episodes

--- pkg/shows/episode.go
+++ pkg/shows/episode.go
@@ -1,11 +1,13 @@
 package shows
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 type Episode struct {
 	Season uint
-	EpisodeNo uint `json:"episode"`
-	Title string `json:"name"`
+	EpisodeNo uint
+	Title string
 }
 
 func (e Episode) MarshalJSON() ([]byte, error) {
@@ -15,4 +17,38 @@ func (e Episode) MarshalJSON() ([]byte, error) {
 		"Title": e.Title,
 	}
 	return json.Marshal(encoded)
+}
+
+func (e *Episode) UnmarshalJSON(b []byte) error {
+	data := map[string]interface{}{}
+	if err := json.Unmarshal(b, &data); err != nil {
+		return err
+	}
+
+	var episodeNo, season float64
+	var title string
+	var exists bool
+
+	episodeNo, exists = data["episode"].(float64)
+	if !exists {
+		episodeNo, exists = data["EpisodeNo"].(float64)
+	}
+	if exists {
+		e.EpisodeNo = uint(episodeNo)
+	}
+
+	season, exists = data["season"].(float64)
+	if exists {
+		e.Season = uint(season)
+	}
+
+	title, exists = data["name"].(string)
+	if !exists {
+		title, exists = data["name"].(string)
+	}
+	if exists {
+		e.Title = title
+	}
+
+	return nil
 }
